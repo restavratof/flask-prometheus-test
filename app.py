239e4d72699e3fde556c1e4bb19442ab969e9190
dfashchanka <Dzmitry.Faschanka@ibm.com>
2021-11-08 17:58:28 +0300

    use prometheus-client

--- app.py
+++ app.py
@@ -4,20 +4,24 @@ import os
 
 from flask import Flask
 from flask import request
-from prometheus_flask_exporter import PrometheusMetrics
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
+from prometheus_client import Gauge
 
-
+# Create my app
 app = Flask(__name__)
-metrics = PrometheusMetrics(app, export_defaults=False)
 
+# Add prometheus wsgi middleware to route /metrics requests
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+
+# --- METRICS ----------------------------------------
 # static information as metric
-metrics.info('app_info', 'Application info', version='0.1.0')
-
-common_counter = metrics.counter(
-    'http_requests_total',
-    'Total number of http requests',
-    labels={'method': lambda: request.method}
-)
+# metrics.info('app_info', 'Application info', version='0.1.0')
+# # Dynamic metric
+load_metric = Gauge('app_load', 'Application load to be used for HPA')
 
 
 # Fibonacci function
@@ -31,20 +35,27 @@ def fibonacci(n):
 
 
 @app.route('/health')
-@metrics.do_not_track()
 def health():
     print(f'health - START')
     return 'OK'
 
 
 @app.route('/fibonacci', methods=['POST'])
-@common_counter
 def fibonacci_route():
     print(f'fibonacci - IN')
-    num = request.get_json()['number']
+    num = request.get_json().get('number')
     result = fibonacci(num)
     print(f'fibonacci - OUT: {result}')
     return f'RESULT: {result}'
+
+
+@app.route('/load', methods=['POST'])
+def load_route():
+    print(f'load_route - IN')
+    val = request.get_json().get('value')
+    load_metric.set(val)
+    print(f'load_route - OUT: {val}')
+    return f'SET'
 
 
 if __name__ == '__main__':
