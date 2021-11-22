d96b71e303d556a900300bdf7b86ad3ed51491d0
dfashchanka <Dzmitry.Faschanka@ibm.com>
2021-11-22 18:36:55 +0300

    in progress

--- test_load.sh
+++ test_load.sh
@@ -5,3 +5,7 @@ for ((i=1; i<=500; i++)); do
   curl -X POST 'http://localhost:5000/fibonacci' -H 'Content-Type: application/json' -d '{"number" : 10}'
   echo ""
 done
+
+
+# --- SET APP LOAD VALUE
+# curl -X POST 'http://localhost:5000/load' -H 'Content-Type: application/json' -d '{"value" : 10}'
